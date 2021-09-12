cc9352aa9b78378a4b3ad54af01b1f98661d16df
Mian-Roshan <55781274+Mian-Roshan@users.noreply.github.com>
2021-09-13 00:46:46 +0500

    video player

--- resources/views/partial/usertest_sync.blade.php
+++ resources/views/partial/usertest_sync.blade.php
@@ -38,33 +38,12 @@
 
         @endphp
         <input class="vtime" type="hidden"  value="{{($time-($resumetime))}}">
-        <input class="vid" type="hidden" value="{{$v->file}}">
        <div style="overflow: auto !important;text-align:center;background: #1e1e2d;">
            <div id="player" ></div>
        </div>
 
-
         <div id="countdown"></div>
-
-
-        <div class="video-control-panel d-flex align-items-center">
-            <button class="bg-transparent border-0" type="button" onclick="pplay()"><i class="bi bi-play"></i></button>
-            <button class="bg-transparent border-0" type="button" onclick="ppause()"><i class="bi bi-pause"></i></button>
-            <button class="bg-transparent border-0" type="button" onclick="sstop()"><i class="bi bi-arrow-counterclockwise"></i></button>
-            <button class="bg-transparent border-0" type="button" onclick="mmute()"><i class="bi bi-volume-mute"></i></button>
-            <button class="bg-transparent border-0" type="button" onclick="unmute()"><i class="bi bi-volume-up"></i></button>
-            <div class="ml-auto d-flex align-items-center">
-                <div class="timer d-sm-block d-none" style="color:#B5B5C3;">
-                    <span class="hour">00</span>:<span class="minute">00</span>:<span class="second">10</span>
-                </div>
-                <div class="range-input d-sm-flex d-none custom__range__field  align-items-center">
-                    <input type="range" min="0" max="100" step="1" value="30" id="myRange" class="mx-3" >
-                </div>
-                <div class="mr-3 d-sm-block d-none">
-                  <span id="demo" class="d-block text-center" style="color:#B5B5C3;width:19px;"></span>
-                </div>
-                <button class="bg-transparent border-0" type="button" onclick="toglefullscreen()"><i class="bi bi-fullscreen"></i></button>
-            </div>    
+        <div class="plyr__video-embed js-player" data-plyr-provider="youtube" data-plyr-embed-id="{{$v->file}}"></div>
         </div>
         @if($video_completed == 1)
             <button type="button" class="btn btn-info video_next" id="countdown" >Next</button>
@@ -92,7 +71,7 @@
             @endif
 
         @php $file=\App\Models\Form::find($v2->form_id); @endphp
-        <iframe src="{{asset('/images/assignments/acknowledgment')}}/{{$file->file_name}}" width="600px" height="2100px" />
+        <iframe src="{{asset('/images/assignments/acknowledgment')}}/{{$file->file_name}}" width="600px" height="2100px"></iframe>
        @endif
        @php $counterr++; @endphp
         @endforeach
@@ -146,252 +125,31 @@
     }
 </style>
 @if($type == 'video')
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/js/plyr/plyr.css')}}" />
+    <script src="{{asset('assets/js/plyr/plyr.min.js')}}"></script>
 <script>
-    // 2. This code loads the IFrame Player API code asynchronously.
-    var tag = document.createElement('script');
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-    // 3. This function creates an <iframe> (and YouTube player)
-    //    after the API code downloads.
-    var player;
-    function onYouTubeIframeAPIReady() {
-        var vid = $('.vid').val();
-        var restime = parseInt($('#resumetime').val());
-        player = new YT.Player('player', {
-
-            videoId: vid,
-
-            playerVars: {
-                fs: 1,
-                controls: 0, // Show pause/play buttons in player
-                showinfo: 0, // Hide the video title
-                disablekb:1,
-                modestbranding: 0, // Hide the Youtube Logo
-                cc_load_policy: 1, // Hide closed captions
-                iv_load_policy: 2, // Hide the Video Annotations
-                start: restime,
-                autohide: 1, // Hide video controls when playing
-            },
-            events: {
-                'onReady': onPlayerReady,
-                'onStateChange': onPlayerStateChange
-            }
+    $(document).ready(() => {
+        const controls = [
+            'play-large', 
+            'restart', 
+            'play', 
+            'duration',
+            'mute',
+            'volume',
+            'download',
+            'fullscreen' 
+        ];
+        const instances = Plyr.setup('.js-player', { controls });
+        var player = instances[0];
+        player.on('ended', function() {
+            $('.video_next').show();
+            $('.next').show();
         });
-    }
-
-    // 4. The API will call this function when the video player is ready.
-    function onPlayerReady(event) {
-        event.target.pauseVideo();
-
-
-    }
-
-
-
-    // 5. The API calls this function when the player's state changes.
-    //    The function indicates that when playing a video (state=1),
-    //    the player should play for six seconds and then stop.
-    var done = false;
-    function onPlayerStateChange(event) {
-
-        if (event.data == YT.PlayerState.PLAYING) {
-            pplay();
-            // setTimeout(stopVideo, 6000);
-            // done = true;
-        }else if(event.data == YT.PlayerState.PAUSED){
-            ppause();
-        }
-
-    }
-    function stopVideo() {
-        player.stopVideo();
-
-    }
-    function pplay() {
-        player.playVideo();
-
-        var timeleft = parseInt($('.vtime').val());
-        timer.start(1000);
-
-    }
-    function sstop() {
-        player.stopVideo();
-        player.playVideo();
-        timer.reset(parseInt($('.vtime').val()));
-
-    }
-    function ppause() {
-        timer.stop();
-        player.pauseVideo();
-    }
-    function mmute() {
-
-        player.mute();
-        console.log(player.getPlayerState());
-    }
-    function unmute() {
-
-        player.unMute();
-
-    }
-    function playFullscreen (){
-
-       return document.FullScreenElement
-            || document.mozFullScreenElement
-            || document.webkitFullScreenElement
-            || document.msFullScreenElement ;
-
-    }
-    function toglefullscreen(){
-
-        if(playFullscreen()){
-            document.exitFullscreen();
-
-        }else{
-            document.getElementById('player').requestFullscreen().catch(console.log);
-        }
-    }
-    // document.addEventListener("dblclick",()=>{
-    //     toglefullscreen();
-    // })
-
-    // requestFullScreen
-
-
-    function _timer(callback)
-    {
-        var time = 0;     //  The default time of the timer
-        var mode = 1;     //    Mode: count up or count down
-        var status = 0;    //    Status: timer is running or stoped
-        var timer_id;    //    This is used by setInterval function
-
-        // this will start the timer ex. start the timer with 1 second interval timer.start(1000)
-        this.start = function(interval)
-        {
-            interval = (typeof(interval) !== 'undefined') ? interval : 1000;
-
-            if(status == 0)
-            {
-                status = 1;
-                timer_id = setInterval(function()
-                {
-                    switch(mode)
-                    {
-                        default:
-                            if(time)
-                            {
-                                time--;
-                                generateTime();
-                                if(typeof(callback) === 'function') callback(time);
-                            }
-                            break;
-
-                        case 1:
-                            if(time < 86400)
-                            {
-                                time++;
-                                generateTime();
-                                if(typeof(callback) === 'function') callback(time);
-                            }
-                            break;
-                    }
-                }, interval);
-            }
-        }
-
-        //  Same as the name, this will stop or pause the timer ex. timer.stop()
-        this.stop =  function()
-        {
-            if(status == 1)
-            {
-                status = 0;
-                clearInterval(timer_id);
-            }
-        }
-
-        // Reset the timer to zero or reset it to your own custom time ex. reset to zero second timer.reset(0)
-        this.reset =  function(sec)
-        {
-            sec = (typeof(sec) !== 'undefined') ? sec : 0;
-            time = sec;
-            generateTime(time);
-        }
-
-        // Change the mode of the timer, count-up (1) or countdown (0)
-        this.mode = function(tmode)
-        {
-            mode = tmode;
-        }
-
-        // This methode return the current value of the timer
-        this.getTime = function()
-        {
-            return time;
-        }
-
-        // This methode return the current mode of the timer count-up (1) or countdown (0)
-        this.getMode = function()
-        {
-            return mode;
-        }
-
-        // This methode return the status of the timer running (1) or stoped (1)
-        this.getStatus
-        {
-            return status;
-        }
-
-        // This methode will render the time variable to hour:minute:second format
-        function generateTime()
-        {
-            var second = time % 60;
-            var minute = Math.floor(time / 60) % 60;
-            var hour = Math.floor(time / 3600) % 60;
-
-            second = (second < 10) ? '0'+second : second;
-            minute = (minute < 10) ? '0'+minute : minute;
-            hour = (hour < 10) ? '0'+hour : hour;
-
-            $('div.timer span.second').html(second);
-            $('div.timer span.minute').html(minute);
-            $('div.timer span.hour').html(hour);
-        }
-    }
-
-    // example use
-    var timer;
-
-
-    var slider = document.getElementById("myRange");
-    var output = document.getElementById("demo");
-
-
-
-    output.innerHTML = slider.value;
-
-    slider.oninput = function() {
-        output.innerHTML = this.value;
-        player.setVolume(this.value);
-    }
-    var timeleft = parseInt($('.vtime').val());
-    timer = new _timer
-    (
-        function(time)
-        {
-            // console.log(player.getPlayerState());
-            if(time == 0)
-            {
-                timer.stop();
-                $('.video_next').show();
-                $('.next').show();
-            }
-        }
-    );
-    timer.reset(timeleft);
-    timer.mode(0);
-
+        $(document).on('click','button[data-plyr="restart"]',function(){
+            $('.video_next').hide();
+            $('.next').hide();
+        })
+    });
 
 </script>
 
