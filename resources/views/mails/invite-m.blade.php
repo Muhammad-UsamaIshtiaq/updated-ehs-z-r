04781c32f52cad97f80e616ef97a4f60f851586a
aliumair <aliumair662@gmail.com>
2021-10-11 21:38:42 +0500

    update mauil

--- resources/views/mails/invite-m.blade.php
+++ resources/views/mails/invite-m.blade.php
@@ -16,7 +16,7 @@
                 <img src="{{asset($temp->img1)}}" style="width:100%;height:auto;" />
             </div>
             <div style="padding:40px 25px;text-align:center;border-width:0px 1px 1px 1px;border-style:solid;border-color:#d2d2d2;">
-                <p style="margin:0px;font-size:16px;">{{$temp->descr}}</p>
+                <p style="margin:0px;font-size:16px;"><?php echo $temp->descr; ?></p>
                 <h3>Your email: {{$email}}</h3>
                 <h3>Your password: {{$password}}</h3>
                 <h3><a href="{{asset('/login')}}">Click Here</a> To Login And Verify Your Account</h3>
@@ -31,7 +31,7 @@
                 </div>
                 <div style="width:65%;padding:25px;box-sizing:border-box;">
                     {{--<h5 style="margin:0;font-size:18px;">What is lorem ipsum</h5>--}}
-                    <p style="margin-bottom:0;font-size:14px;">{{asset($temp->body2)}}</p>
+                    <p style="margin-bottom:0;font-size:14px;"><?php echo $temp->body2; ?></p>
                     <a href="{{$temp->img2_button}}" style="border:none;font-size:14px;color:#fff;background:#000;padding:10px 20px;text-decoration:none;
                         display:inline-block;margin-top:20px;">
                         Click Here
@@ -44,7 +44,7 @@
                 </div>
                 <div style="width:65%;padding:25px;box-sizing:border-box;">
                     {{--<h5 style="margin:0;font-size:18px;">What is lorem ipsum</h5>--}}
-                    <p style="margin-bottom:0;font-size:14px;">{{asset($temp->body3)}}</p>
+                    <p style="margin-bottom:0;font-size:14px;"><?php echo $temp->body3; ?></p>
                     <a href="{{$temp->img3_button}}" style="border:none;font-size:14px;color:#fff;background:#000;padding:10px 20px;text-decoration:none;
                         display:inline-block;margin-top:20px;">
                         Click Here
@@ -53,7 +53,7 @@
             </div>
             <div style="background:url({{asset($temp->bannerImg)}}) no-repeat center;">
                 <div style="padding:40px 25px;text-align:center;background:rgb(0,0,0,0.5);">
-                    <p style="margin:0px;color:#fff;font-size:16px;">{{asset($temp->bannerText)}}</p>
+                    <p style="margin:0px;color:#fff;font-size:16px;"><?php echo $temp->bannerText; ?></p>
                 </div>
             </div>
         </div>
